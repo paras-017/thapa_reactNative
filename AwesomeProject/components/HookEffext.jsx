9c0dd428655179e7be05d31a4583e17f23baad26
paras-017 <paraschauhan0017@gmail.com>
2023-08-15 14:19:39 +0530

    v:27 useeffect in RN

---
+++ AwesomeProject/components/HookEffext.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HookEffext = () => {
+  return (
+    <View>
+      <Text>HookEffext</Text>
+    </View>
+  )
+}
+
+export default HookEffext
+
+const styles = StyleSheet.create({})
